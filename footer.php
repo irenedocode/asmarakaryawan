118ea7fd50cac32e39346ffb275fec2ad2c08e1f
Irene <irenetheresha@gmail.com>
2024-02-28 16:40:01 +0700

    proses formulir pemesanan

--- footer.php
+++ footer.php
@@ -1,6 +1,23 @@
-<footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
+
+
+<style>
+
+.footer {
+    background-color: #050650;
+    height: 150px;
+    text-align: center;
+}
+
+.text {
+    position: relative;
+    top: 40%;
+}
+
+</style>
+
+<footer class="footer" style="margin-top: 50px;">
+                <div class="footer">
+                    <div class="text">
                         <span>Copyright &copy; Asmara Group</span>
                     </div>
                 </div>
